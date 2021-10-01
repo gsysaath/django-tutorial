ac0fcbc29456c8d6bb6262f384e54365260a4e66
georges sysaath <georges.sysaath@gmail.com>
2021-10-01 11:35:27 +0200

    bugs step 3 and 4

--- polls/urls.py
+++ polls/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls import url
 
 from . import views
 
+app_name = 'polls'
 urlpatterns = [
     url(r'', views.index, name='index'),
+    url(r'specifics/<int:question_id>/', views.detail, name='detail'),
+    url(r'<int:question_id>/results/', views.results, name='results'),
+    url(r'<int:question_id>/vote/', views.vote, name='vote'),
 ]
